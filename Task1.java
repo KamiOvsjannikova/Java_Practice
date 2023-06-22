227a744f25ff771edd79e6a9227d305983b6b566
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-22 14:47:27 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,47 @@
+package org.telran;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int myInt = 3;
+        int myInt2 = 10;
+        System.out.println(myInt);
+        System.out.println(myInt2);
+
+        int resultInt = myInt + myInt2;
+        System.out.println(resultInt);
+
+        System.out.println(myInt + myInt2);
+        System.out.println(myInt - myInt2);
+        System.out.println(myInt * myInt2);
+        System.out.println(myInt2 / myInt);
+        System.out.println(myInt2 % myInt);
+
+        double myDouble = 3.0;
+        double myDouble2 = 10;
+
+        double resultDouble = (double)myDouble2 / myDouble;
+        System.out.println(resultDouble);
+
+
+        byte myByte = 66;
+        float myFloat = 33.01F;
+        System.out.println(myInt);
+        System.out.println(myFloat);
+        System.out.println();
+
+
+
+
+
+        System.out.print("Hello Kamilla");
+        System.out.print("5555");
+        System.out.println(" 11111");
+        System.out.println(" 5555");
+        myInt = 6;
+        System.out.println(myInt);
+        runFast();
+    }
+    static void runFast() {
+        System.out.println("Runner!!!");
+    }
+}
