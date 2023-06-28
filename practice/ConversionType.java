2e7ccb98283f1d6bf196e292d886daa1388a9621
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-28 21:19:48 +0300

    Add files via upload

---
+++ practice/ConversionType.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class ConversionType {
+
+        public static void main(String[] args) {
+            byte myByte = 64;
+            int myInt = myByte;
+
+            long myLong = 4566;
+            myByte = (byte)myLong;
+            System.out.println(myLong);
+            System.out.println(myByte);
+        }
+    }
+
