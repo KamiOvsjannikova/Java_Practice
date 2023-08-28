362566ef9add96cba6c9a583751ecac24383c922
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-08-28 13:22:11 +0300

    Add files via upload

---
+++ Lesson3/src/main/java/practice/PracticeMain.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class PracticeMain {
+    static int myValueClass = 3;
+
+    public static void main(String[] args) {
+        int myInt2 = 2;
+        our();
+        System.out.println(myInt2);
+        System.out.println(myValueClass+1);
+
+        Test myTest = new Test();
+        Test myTest33 = myTest;
+        myTest.run();
+        System.out.println(myTest.myIntTest + " - " + Test.myIntSatic);
+        Test.myIntSatic = 999;
+        Test myTest1 = new Test();
+        myTest1.myIntTest = 88;
+        System.out.println(myTest1.myIntTest + " - " + Test.myIntSatic);
+        Test myTest2 = new Test();
+        myTest2.myIntTest = 99;
+        System.out.println(myTest2.myIntTest + " - " + Test.myIntSatic);
+    }
+    static void our() {
+        int myInt = 1;
+        System.out.println("I method our()");
+        System.out.println(myInt);
+        System.out.println(myValueClass+2);
+    }
+
+}
---
+++ Lesson3/src/main/java/practice/Test.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Test {
+    static int myIntSatic = 33;
+    public int myIntTest = 77;
+    public void run() {
+
+        System.out.println("Test -> run()");
+  }
+}
