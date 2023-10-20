75afe1c55f57ae32a57ac317e5638d2ccb8289f2
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-10-20 13:21:04 +0300

    Update PolyExampleTwo.java

--- telranqa/src/main/java/LessonQA201023/PolyExampleTwo.java
+++ telranqa/src/main/java/LessonQA201023/PolyExampleTwo.java
@@ -3,6 +3,7 @@ package LessonQA201023;
 import java.util.Arrays;
 
 public class PolyExampleTwo {
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         String[] strings = {"One", "Two", "Three"};
@@ -10,6 +11,34 @@ public class PolyExampleTwo {
         Arrays.sort(arr);
         Arrays.sort(strings);
 
+        /*
+        Arrays.sortInt(arr);
+        Arrays.sortString(strings);
+         */
 
+        printArray(arr);
+        //printStringArray(strings); // - wrong way
+        printArray(strings);
+    }
+
+    public static void printArray(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printStringArray(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
