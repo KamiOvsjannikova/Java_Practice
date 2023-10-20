f2cbcff84d8eb322205c9715dbdab3410070cafe
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-10-20 13:20:42 +0300

    Update PrintExampleArray.java

--- telranqa/src/main/java/LessonQA201023/PrintExampleArray.java
+++ telranqa/src/main/java/LessonQA201023/PrintExampleArray.java
@@ -1,11 +1,12 @@
 package LessonQA201023;
 
 public class PrintExampleArray {
-    public static void main(String[] args) {
-        String[] str = { "Two", "Four"};
-       // PolyExampleTwo.printArray(str);
 
-      //  int[] = ints = { 4,5,6,4};
-       // PolyExampleTwo.printArray(ints);
+    public static void main(String[] args) {
+        String[] str  = {"Two","Four"};
+        PolyExampleTwo.printArray(str);
+
+        int[] ints = {4,5,6,4};
+        PolyExampleTwo.printArray(ints);
     }
 }
