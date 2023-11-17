005fc53e02cb1d10fb52baa2f7a90c0f0900b94e
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-11-17 20:26:38 +0200

    Update MyLinkedList.java

--- telranqa/src/main/java/LessonQA171123/MyLinkedList.java
+++ telranqa/src/main/java/LessonQA171123/MyLinkedList.java
@@ -1,6 +1,6 @@
 package LessonQA171123;
 
-public class MyLinkedList { // svjazannij spisok- eto nabor 1-ogo i togo ze tipa, kotorij ssilaetsja na drugie elemnti
+public class MyLinkedList { // svjazannij spisok- eto nabor 1-ogo i togo ze tipa, kotorij ssilaetsja na drugie elemnti. MyLinkedList - dvustoronnej spisok emplimentiruet
     private Node head;
 
     //Time complexity 0(1)
