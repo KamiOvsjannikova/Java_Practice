1b8eb4d1208b05e50d3d54e68ac5837b088244f1
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-12-08 13:54:43 +0200

    Update MyDeque.java

--- telranqa/src/main/java/lessonQA0812/queue/MyDeque.java
+++ telranqa/src/main/java/lessonQA0812/queue/MyDeque.java
@@ -1,7 +1,13 @@
 package lessonQA0812.queue;
 
 public interface MyDeque {
+    //FIFO - first input first output - 1-ij priwel, 1-ij uwel
     // -> 4 3 2 1 0->
+    // 0 1 2 3 4
+    // 5 4 3 2 1 0
+    //ArrayDeque - na algoritmax rasmatrivatj budem
+
+
     boolean isEmpty();
     void addToTail(Integer element) ;// v xvost dobavitj elementi
     Integer removeHead();
